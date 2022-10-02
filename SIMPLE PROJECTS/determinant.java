22fb154ab58b25ac38b931806236cbb76a3f0cd1
Madhav Saini <99039829+FireNdIce3@users.noreply.github.com>
2022-10-02 13:23:53 +0530

    Update determinant.java

--- SIMPLE PROJECTS/determinant.java
+++ SIMPLE PROJECTS/determinant.java
@@ -1,3 +1,11 @@
+// Question: Find the determinant of nxn matrix 
+//  Example Input : 3 (Dimension of matrix)
+// 1 2 3
+// 4 5 6 
+// 7 8 9
+// Output = 0
+
+
 // gives determinant of nxn matrix
 package com.company;
 
