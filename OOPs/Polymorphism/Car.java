ec6a83d2b83c3b7262cb577439167111e6cdbcf7
shivay10th <shivay9455@gmail.com>
2022-10-09 18:02:53 +0530

    Some comments added

--- OOPs/Polymorphism/Car.java
+++ OOPs/Polymorphism/Car.java
@@ -3,12 +3,33 @@ public class Car {
     private int cylinders;
     private String name;
     private int wheels;
+    private String color;
+
+    /* Constructor overloading is a very good example of Mehod Overloading */
+    /*
+     * In Method Overloading we can use the same method name with a different
+     * signature
+     * that means with a different number of parameters or a different return type.
+     */
+
+    public Car(int cylinders, String name, String color) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.color = color;
+        this.engine = true;
+        this.wheels = 4;
+    }
 
     public Car(int cylinders, String name) {
         this.cylinders = cylinders;
         this.name = name;
         this.engine = true;
         this.wheels = 4;
+        this.color = "White";
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public int getCylinders() {
--- OOPs/Polymorphism/Mitsubishi.java
+++ OOPs/Polymorphism/Mitsubishi.java
@@ -1,8 +1,15 @@
 public class Mitsubishi extends Car {
 
     public Mitsubishi(int cylinders, String name) {
+        /* The super keyword is used to call the parent class constructor */
         super(cylinders, name);
     }
+
+    /*
+     * This is the example of a Method overriding
+     * There is one condition with method overriding we can't change the signature
+     * of a method but we can change the implementation
+     */
 
     @Override
     public String accelerate() {
