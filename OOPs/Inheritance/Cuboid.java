7050289a599a117c3e8dc9e46ad6334e93b93be8
shivay10th <shivay9455@gmail.com>
2022-10-09 17:35:57 +0530

    Example for inheritance added

---
+++ OOPs/Inheritance/Cuboid.java
@@ -0,0 +1,26 @@
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    /*
+     * Method overriding
+     */
+
+    @Override
+    public void thisIS() {
+        System.out.println("This is " + this.getClass() + " and My Parent class is " + super.getClass());
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return this.getArea() * this.height;
+    }
+
+}
